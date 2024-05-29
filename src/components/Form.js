d685296dee46fa051406cb4a2b5e6cc73d865a74
ellie-ctrl35 <146957037+ellie-ctrl35@users.noreply.github.com>
2024-05-29 12:28:41 +0100

    form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,16 +2,29 @@ import { useState } from 'react';
 import { EyeIcon, EyeOffIcon, MailIcon } from '@heroicons/react/outline';
 import Logo from '../assests/Logo.svg';
 import SubmitButton from './SubmitButton';
+import {ToastContainer,toast } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 const Form = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [email,setEmail] = useState("");
+  const [password,setPassword]= useState("");
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email === "") {
+      toast.info("Please enter an email"); // Display error toast
+    } else if (password === "") {
+      toast.info("Please enter your password");
+    }
+  };
+
   return (
-    <form className='flex flex-col gap-4 bg-white p-8 rounded-lg w-96 mx-auto'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4 bg-white p-8 rounded-lg w-96 mx-auto'>
       <div className="flex justify-center mb-4 bg-sky-800 p-2 rounded-lg w-32 mx-auto">
         <img src={Logo} alt="Logo" className="w-24 h-10" />
       </div>
@@ -21,12 +34,14 @@ const Form = () => {
       <div className="flex flex-col">
         <label htmlFor="email" className="text-gray-600 mb-2">Email</label>
         <div className="flex items-center border border-gray-400 rounded-md p-2 w-75">
-          <MailIcon className="text-gray-500 h-4 w-4 mr-2" />
+          <MailIcon className="text-gray-500 h-5 w-5 mr-2" />
           <input
             id="email"
             type="text"
             placeholder="example@gmail.com"
             className="flex-1 bg-transparent outline-none"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
           />
         </div>
       </div>
@@ -38,17 +53,20 @@ const Form = () => {
             type={showPassword ? "text" : "password"}
             placeholder="Password"
             className="flex-1 bg-transparent outline-none font-sans"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
           />
           {showPassword ? (
-            <EyeOffIcon className="text-gray-500 h-4 w-4 ml-2 cursor-pointer" onClick={togglePasswordVisibility} />
+            <EyeOffIcon className="text-gray-500 h-5 w-5 ml-2 cursor-pointer" onClick={togglePasswordVisibility} />
           ) : (
-            <EyeIcon className="text-gray-500 h-4 w-4 ml-2 cursor-pointer" onClick={togglePasswordVisibility} />
+            <EyeIcon className="text-gray-500 h-5 w-5 ml-2 cursor-pointer" onClick={togglePasswordVisibility} />
           )}
         </div>
         <div className="flex justify-center mt-8 bg-sky-800 p-1 rounded-md">
           <SubmitButton text="Log In" width="100%" height="40px" className="w-full" />
         </div>
       </div>
+      <ToastContainer/>
     </form>
   );
 };
