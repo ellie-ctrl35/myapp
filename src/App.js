2fa7f88915f94b1d6a3bc8b4a6f06f2caf1453df
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-10 01:23:53 +0000

    Tabs Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { Tabs, Tab } from '@mui/material';
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Transaction from "./pages/Transactions/Transaction";
@@ -6,9 +7,29 @@ import DefaultLayout from "./components/DefaultLayout";
 import Confirmations from './pages/Transactions/Confirmations';
 import AddTransactions from './pages/Transactions/AddTransactions';
 
+const TransactionsTabs = () => {
+  const location = useLocation();
+  const currentTab = location.pathname.split('/')[2] || 'confirmed-balances';
+
+  return (
+    <div>
+      <Tabs value={currentTab} aria-label="transactions tabs">
+        <Tab label="Confirmed Balances" value="confirmed-balances" component={Link} to="/transactions/confirmed-balances" />
+        <Tab label="Unconfirmed Balances" value="unconfirmed-balances" component={Link} to="/transactions/unconfirmed-balances" />
+        <Tab label="Add Transactions" value="add-transactions" component={Link} to="/transactions/add-transactions" />
+      </Tabs>
+      <Routes>
+        <Route path="confirmed-balances" element={<Confirmations />} />
+        <Route path="unconfirmed-balances" element={<Transaction />} />
+        <Route path="add-transactions" element={<AddTransactions />} />
+      </Routes>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route
@@ -23,12 +44,12 @@ function App() {
           path="/transactions/*"
           element={
             <DefaultLayout>
-                <Route path="/" element={<Transaction />} />
+              <TransactionsTabs />
             </DefaultLayout>
           }
         />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
